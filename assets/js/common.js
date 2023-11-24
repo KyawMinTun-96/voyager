a250f6dfb546442f361ad1fd6ccdf01f4a29f0ad
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2023-11-25 03:07:08 +0630

    UI changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,21 @@
 var btn = document.querySelector('.btnGearUP');
+var arr = document.querySelector('.btnArrow');
+var arr2 = document.querySelector('.btnArrow2');
 
 
 btn.addEventListener('click', function() {
+    console.log(btn);
+    alert("Hello World");
+})
+
+
+arr.addEventListener('click', function() {
+    console.log(btn);
+    alert("Hello World");
+})
+
+
+arr2.addEventListener('click', function() {
     console.log(btn);
     alert("Hello World");
 })
